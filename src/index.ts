1663703a3e80d4f80aadc4e298c6a534379d9b26
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2025-02-08 10:26:34 -0300

    change addy

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { performRun } from './openai/performRun';
 
 async function main() {
     const client = new OpenAi();
-    const message = "send .01 to the address 0x17e968F5C0472941767F06b660Ab2E7149Bdf7ED and then tweet about it but end tweet with the number 5";
+    const message = "send .01 to the address  and then tweet about it but end tweet with the number 5";
     const assistant = await createAssistant(client);
     const thread = await createThread(client, message);
     const run = await createRun(client, thread, assistant.id);
