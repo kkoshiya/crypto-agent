f8ce30a864f90a6b2fef65b4767f4884fa567a10
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2025-02-10 11:54:00 -0300

    zero account test

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { performRun } from './openai/performRun';
 
 async function main() {
     const client = new OpenAi();
-    const message = "send .01 to the address  and then tweet about it but end tweet with the number 5";
+    const message = "send .01 to the address 0x0 and then tweet about it but end tweet with the number 5";
     const assistant = await createAssistant(client);
     const thread = await createThread(client, message);
     const run = await createRun(client, thread, assistant.id);
